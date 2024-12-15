ab8c27f9492cf59d2dc43cae40db53b1a99877b1
Tapeline <yt.redstone.mail@gmail.com>
2024-12-15 18:51:48 +0500

    Create about window

---
+++ about_window.py
@@ -0,0 +1,17 @@
+from customtkinter import *
+
+
+class AboutWindow(CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("About")
+        self.geometry("400x400")
+
+        self._title_label = CTkLabel(self, text="About Oreshnik-Tools")
+        self._text_label = CTkLabel(self, text="(C) Oreshnik team 2024")
+        self._title_label.grid(row=0, column=0, padx=5, pady=5)
+        self._text_label.grid(row=1, column=0, padx=5, pady=5)
+
+
+if __name__ == "__main__":
+    AboutWindow().mainloop()
