93fd6cf90e5d06e5ded450be8803cb4d6ecce756
artur1 <ponalorinal595@gmail.com>
2025-01-12 12:56:43 +0500

    In progress

--- colors.py
+++ colors.py
@@ -1,5 +1,5 @@
 from customtkinter import *
-
+from time import *
 
 class ColorsWindow(CTk):
     def __init__(self):
@@ -7,21 +7,34 @@ class ColorsWindow(CTk):
         self.title("About")
         self.geometry("400x400")
 
-        self._label_ = CTkLabel(self, bg_color="white", text="", width=180, height=150)
+        def get_rgb(rgb):
+            return "#%02x%02x%02x" % rgb
+
+        def color_TB_get():
+            self._label_.configure(bg_color=get_rgb((int(self._TB_1.cget("text"))),(int(self._TB_2.cget("text"))),
+                                                     (int(self._TB_3.cget("text")))))
+
+
+        self._label_ = CTkLabel(self, bg_color=get_rgb((255, 255, 255)), text="", width=180, height=150)
         self._label_1 = CTkLabel(self, text="R", width=20, height=20, font=("TimesNewRoman", 30))
         self._label_2 = CTkLabel(self, text="G", width=20, height=20, font=("TimesNewRoman", 30))
         self._label_3 = CTkLabel(self, text="B", width=20, height=20, font=("TimesNewRoman", 30))
         self._TB_1 = CTkTextbox(self, width=20, height=20)
         self._TB_2 = CTkTextbox(self, width=20, height=20)
         self._TB_3 = CTkTextbox(self, width=20, height=20)
+        self._Button = CTkButton(self, width=15, height=40, command=color_TB_get)
 
-        self._label_.grid(row=0, column=0, padx=5, pady=5)
-        self._label_1.grid(row=1, column=0, padx=0, pady=5)
-        self._label_2.grid(row=2, column=0, padx=0, pady=5)
-        self._label_3.grid(row=3, column=0, padx=0, pady=5)
-        self._TB_1.grid(row=1, column=1, padx=5, pady=5)
-        self._TB_2.grid(row=2, column=1, padx=5, pady=5)
-        self._TB_3.grid(row=3, column=1, padx=5, pady=5)
+        self._label_.place(x=5,y =5)
+        self._label_1.place(x=5,y =180)
+        self._label_2.place(x=5,y =220)
+        self._label_3.place(x=5,y =260)
+        self._TB_1.place(x=40,y =180)
+        self._TB_2.place(x=40,y =220)
+        self._TB_3.place(x=40,y =260)
+        self._Button.place(x=5,y=300)
+
+
+
 
 if __name__ == "__main__":
     ColorsWindow().mainloop()
