50386d1c5c82899869b4d498be15218cb6d4ede0
Saddam-Husein-Oreshnik <ponalprinal595@gmail.com>
2025-01-12 18:57:40 +0500

    Final

--- colors.py
+++ colors.py
@@ -1,43 +1,32 @@
 from customtkinter import *
 from time import *
-
+from tkinter.colorchooser import askcolor
 
 class ColorsWindow(CTk):
     def __init__(self):
         super().__init__()
-        self.title("About")
-        self.geometry("400x400")
+        self.title("ColorPeaker")
+        self.geometry("200x260")
 
         def get_rgb(rgb):
             return "#%02x%02x%02x" % rgb
 
-        def color_TB_get():
-            self._label_.configure(bg_color=get_rgb((int(self._TB_1.get("0.0", "end")),
-                                                     int(self._TB_2.get("0.0", "end")),
-                                                     int(self._TB_3.get("0.0", "end")))))
+        def change_color():
+            colors = askcolor(title="Tkinter Color Chooser")
+            self._TB_1.delete("0.0", "end")
+            self._TB_1.insert("end", colors[1])
+            self._label_.configure(bg_color=colors[1])
 
         self._label_ = CTkLabel(self, bg_color=get_rgb((0, 0, 0)), text="", width=180, height=150)
-        self._label_1 = CTkLabel(self, text="R", width=20, height=20, font=("TimesNewRoman", 30))
-        self._label_2 = CTkLabel(self, text="G", width=20, height=20, font=("TimesNewRoman", 30))
-        self._label_3 = CTkLabel(self, text="B", width=20, height=20, font=("TimesNewRoman", 30))
-        self._TB_1 = CTkTextbox(self, width=40, height=20)
-        self._TB_2 = CTkTextbox(self, width=40, height=20)
-        self._TB_3 = CTkTextbox(self, width=40, height=20)
-        self._Button = CTkButton(self, width=15, height=40, command=color_TB_get, text="Получить цвет",
+        self._TB_1 = CTkTextbox(self, width=180, height=20)
+        self._Button = CTkButton(self, width=15, height=40, command=change_color, text="Открыть colorpeaker",
                                  font=("TimesNewRoman", 16))
 
-        self._TB_1.insert("end", "0")
-        self._TB_2.insert("end", "0")
-        self._TB_3.insert("end", "0")
+        #self._TB_1.insert("end", "0")
 
         self._label_.place(x=5,y =5)
-        self._label_1.place(x=5,y =180)
-        self._label_2.place(x=5,y =220)
-        self._label_3.place(x=5,y =260)
-        self._TB_1.place(x=40,y =180)
-        self._TB_2.place(x=40,y =220)
-        self._TB_3.place(x=40,y =260)
-        self._Button.place(x=5,y=300)
+        self._TB_1.place(x=5,y =170)
+        self._Button.place(x=5,y=210)
 
 
 if __name__ == "__main__":
