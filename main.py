ae8eb8b68af90c72ef4799f401b5516b563ec000
Tapeline <yt.redstone.mail@gmail.com>
2025-01-12 18:17:11 +0500

    Add any base to any base and qr gen to main window.

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 from customtkinter import *
 
+import SySs
 from Text_Analysis import TextAnalysisWindow
 from about_window import AboutWindow
+from qr_window import QRWindow
 from regex_window import RegexWindow
 
 
@@ -28,11 +30,11 @@ class MainWindow(CTk):
         self._text_btn = CTkButton(self, text="Text_analys", command=self._open_text)
         self._text_btn.grid(row=3, column=0)
 
-        self._about_btn = CTkButton(self, text="trsnsSS", command=self._open_about)
-        self._about_btn.grid(row=4, column=0)
+        self._trans_btn = CTkButton(self, text="any base to any base", command=self._open_transSS)
+        self._trans_btn.grid(row=4, column=0)
 
-        self._about_btn = CTkButton(self, text="unixTMSHTMP", command=self._open_about)
-        self._about_btn.grid(row=5, column=0)
+        self._qr_btn = CTkButton(self, text="QR gen", command=self._open_qr_gen)
+        self._qr_btn.grid(row=5, column=0)
 
     def _open_about(self):
         AboutWindow().mainloop()
@@ -44,10 +46,10 @@ class MainWindow(CTk):
         TextAnalysisWindow().mainloop()
 
     def _open_transSS(self):
-        RegexWindow().mainloop()
+        SySs.start_window()
 
-    def _open_UnixTTMP(self):
-        RegexWindow().mainloop()
+    def _open_qr_gen(self):
+        QRWindow().mainloop()
 
 
 def main():
